ed490be0abb2825d4b90d2b9c5b58dd5168b0b6f
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-08-06 11:31:40 +1000

    importing validation middleware to categoryRouter and testing with rest.http

--- src/middlewares/joi-validation/productCategoryValidation.js
+++ src/middlewares/joi-validation/productCategoryValidation.js
@@ -1,4 +1,14 @@
-import { LONGSTR, SHORTSTR } from "./constantValidation.js";
+import Joi from 'joi'
+import { LONGSTR, SHORTSTR, VALIDATOR } from "./constantValidation.js";
 
-
-
+export const newCategoryValidation = (req, res, next) => {
+    try {
+        const schema = Joi.object({
+            parentCat: SHORTSTR.allow(""),
+            catName: SHORTSTR.required()
+        })
+        VALIDATOR(schema, req, res, next)
+    } catch (error) {
+        next(error) // error will be status 200 but message will be given forward
+    }
+}
--- src/routers/categoryRouter.js
+++ src/routers/categoryRouter.js
@@ -1,9 +1,10 @@
 import express from "express"
+import { newCategoryValidation } from "../middlewares/joi-validation/productCategoryValidation.js"
 const router = express.Router()
 
-router.post("/", (req, res, next) => {
+router.post("/", newCategoryValidation, (req, res, next) => {
     try {
-        console.log(req.body)
+        console.log(req.body) // if we get a console log here, we pass the validation, else we fail
     } catch (error) {
         console.log(error)
         error.status = 500
