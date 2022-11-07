eb65a0cbd3e1b5d7c36af7af562b5844f92bcf2c
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-11-08 06:29:41 +1100

    serverside validation

--- src/middlewares/joi-validation/productCategoryValidation.js
+++ src/middlewares/joi-validation/productCategoryValidation.js
@@ -1,5 +1,5 @@
 import Joi from 'joi'
-import { LONGSTR, SHORTSTR, VALIDATOR } from "./constantValidation.js";
+import { LONGSTR, SHORTSTR, VALIDATOR, PRICE, DATE, QTY } from "./constantValidation.js";
 
 export const newCategoryValidation = (req, res, next) => {
     try {
@@ -15,3 +15,24 @@ export const newCategoryValidation = (req, res, next) => {
         next(error) // error will be status 200 but message will be given forward
     }
 }
+
+export const newProductValidation = (req, res, next) => {
+    try {
+        console.log("validation")
+        const schema = Joi.object({
+            _id: SHORTSTR.allow(""),
+            status: SHORTSTR,
+            name: SHORTSTR.required(),
+            sku: SHORTSTR.required(),
+            description: LONGSTR.required(),
+            qty: QTY.required(),
+            price: PRICE.required(),
+            salesPrice: PRICE,
+            salesDate: DATE.allow(null),
+        })
+        VALIDATOR(schema, req, res, next)
+    } catch (error) {
+        next(error) // error will be status 200 but message will be given forward
+    }
+}
+
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
+import { newProductValidation } from '../middlewares/joi-validation/productCategoryValidation.js'
 
 const router = express.Router()
 
-router.post('/', (req, res, next) => {
+router.post('/', newProductValidation, (req, res, next) => {
     try {
-        console.log(req.body)
+        console.log(req.body, "displaying from productRouter")
         res.json({
             status: 'success',
             message: 'todo',
